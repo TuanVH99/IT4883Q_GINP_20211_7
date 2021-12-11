085f2f49d222e9b305a5ac42052e33d3fb972d2f
DESKTOP-QM1U137\tiennd <tien1241999@gmail.com>
2021-12-11 16:01:02 +0700

    update UI, register and login pages but not fully functional

--- be-api/middlewares/authJwt.js
+++ be-api/middlewares/authJwt.js
@@ -5,53 +5,53 @@ const db = require("../models");
 const User = db.user;
 
 verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+    let token = req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(403).send({
-      message: "No token provided!",
-    });
-  }
-
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({
-        message: "Unauthorized",
-      });
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!",
+        });
     }
 
-    req.userId = decoded.id;
-    next();
-  });
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(401).send({
+                message: "Unauthorized",
+            });
+        }
+
+        req.userId = decoded.id;
+        next();
+    });
 };
 
 isAdmin = (req, res, next) => {
-  User.findByPk(req.userId)
-    .then((user) => {
-      user.getRoles().then((roles) => {
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "admin") {
-            next();
-            return;
-          }
-        }
-        res.status(403).send({
-          message: "Require admin role!",
+    User.findByPk(req.userId)
+        .then((user) => {
+            user.getRoles().then((roles) => {
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
+                        next();
+                        return;
+                    }
+                }
+                res.status(403).send({
+                    message: "Require admin role!",
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send({
+                message: "Something wrong, please try again!",
+            });
         });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        message: "Something wrong, please try again!",
-      });
-    });
 };
 
 const authJwt = {
-  verifyToken: verifyToken,
-  isAdmin: isAdmin,
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
 };
 
 module.exports = authJwt;
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,47 +1,122 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/add-tutorial.component";
-import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
+import AuthService from "./services/auth.service";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
-            bezKoder
-          </a>
-          <div className="navbar-nav mr-auto">
+// import AddTutorial from "./components/add-tutorial.element";
+// import Tutorial from "./components/tutorial.element";
+// import TutorialsList from "./components/tutorials-list.element";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BoardUser from "./components/BoardUser";
+import BoardModerator from "./components/BoardModerator";
+import BoardAdmin from "./components/BoardAdmin";
+
+const App = () => {
+  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
+      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/"} className="navbar-brand">
+          bezKoder
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/home"} className="nav-link">
+              Home
+            </Link>
+          </li>
+
+          {showModeratorBoard && (
             <li className="nav-item">
-              <Link to="/tutorials" className="nav-link">
-                Tutorials
+              <Link to={"/mod"} className="nav-link">
+                Moderator Board
+              </Link>
+            </li>
+          )}
+
+          {showAdminBoard && (
+            <li className="nav-item">
+              <Link to={"/admin"} className="nav-link">
+                Admin Board
+              </Link>
+            </li>
+          )}
+
+          {currentUser && (
+            <li className="nav-item">
+              <Link to={"/user"} className="nav-link">
+                User
+              </Link>
+            </li>
+          )}
+        </div>
+
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/add" className="nav-link">
-                Add
+              <a href="/login" className="nav-link" onClick={logOut}>
+                LogOut
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Sign Up
               </Link>
             </li>
           </div>
-        </nav>
+        )}
+      </nav>
 
-        <div className="container mt-3">
-          <Routes>
-            <Route path="/*" element={<TutorialsList />} />
-            <Route path="/tutorials//" element={<TutorialsList />} />
-            <Route path="/add/*" element={<AddTutorial />} />
-            <Route path="/tutorials/:id" element={<Tutorial />} />
-          </Routes>
-        </div>
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path={"/"} element={<Home />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/profile" element={<Profile />}></Route>
+          <Route path="/user" element={<BoardUser />}></Route>
+          <Route path="/mod" element={<BoardModerator />}></Route>
+          <Route path="/admin" element={<BoardAdmin />}></Route>
+        </Routes>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
