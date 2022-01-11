50bb72cd8f58fe9aa5b25e1c526911ba6ee265d0
DESKTOP-QM1U137\tiennd <tien1241999@gmail.com>
2022-01-11 07:46:14 +0700

    making some changes, users are now able to chat in realtime in FE but not yet able to send private msg to each other

--- be1/socket/index.js
+++ be1/socket/index.js
@@ -1,40 +1,41 @@
-module.exports = function (http) {
-  const io = require("socket.io")(http, {
-    cors: {
-      origin: "*",
-    },
-  });
-  io.on("connection", function (socket) {
-    console.log("An user has connected!");
-    // console.log(socket);
-    //----------user------------
-    socket.on("userOnline", (data) => {
-      console.log(data);
+module.exports = function(http) {
+    const io = require("socket.io")(http, {
+        cors: {
+            origin: "*",
+        },
     });
-    //----------room------------
-    socket.on("userJoinPrivateRoom", (data) => {
-      console.log("some user open a private chart, private chat data:");
-      console.log(data);
-      socket.join(data.roomid);
+    io.on("connection", function(socket) {
+        console.log("An user has connected!");
+        // console.log(socket);
+        //----------user------------
+        socket.on("userOnline", (data) => {
+            console.log(data);
+        });
+        //----------room------------
+        socket.on("userJoinPrivateRoom", (data) => {
+            console.log("some user open a private chart, private chat data:");
+            console.log(data);
+            socket.join(data.roomid);
+        });
+        //----------message------------
+        socket.on("sendPrivateMessage", (data) => {
+            console.log("some user send private message, roomid:");
+            console.log(data["to_room_id"]);
+            io.emit("message", data)
+            io.to(data["to_room_id"]).emit("newPrivateMessage", data);
+        });
+        //----------Call------------
+        socket.on("want4Call", (data) => {
+            console.log("some user want to make a voice call!");
+            socket.broadcast.to(data.room).emit("ready4Call", data);
+        });
+        socket.on("ok2Call", (data) => {
+            console.log("ready for call");
+            socket.broadcast.to(data.room).emit("canCall", data);
+        });
+        //-----------------------------
+        socket.on("disconnect", function() {
+            console.log("An user has disconnected");
+        });
     });
-    //----------message------------
-    socket.on("sendPrivateMessage", (data) => {
-      console.log("some user send private message, roomid:");
-      console.log(data["to_room_id"]);
-      io.to(data["to_room_id"]).emit("newPrivateMessage", data);
-    });
-    //----------Call------------
-    socket.on("want4Call", (data) => {
-      console.log("some user want to make a voice call!");
-      socket.broadcast.to(data.room).emit("ready4Call", data);
-    });
-    socket.on("ok2Call", (data) => {
-      console.log("ready for call");
-      socket.broadcast.to(data.room).emit("canCall", data);
-    });
-    //-----------------------------
-    socket.on("disconnect", function () {
-      console.log("An user has disconnected");
-    });
-  });
 };
--- ui/src/App.js
+++ ui/src/App.js
@@ -10,13 +10,8 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import BoardUser from "./components/BoardUser";
-import BoardModerator from "./components/BoardModerator";
-import BoardAdmin from "./components/BoardAdmin";
 
 const App = () => {
-  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
   const [currentUser, setCurrentUser] = useState(undefined);
 
   useEffect(() => {
@@ -31,79 +26,70 @@ const App = () => {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <Link to={"/"} className="navbar-brand">
-          bezKoder
-        </Link>
+          bezKoder{" "}
+        </Link>{" "}
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link to={"/home"} className="nav-link">
-              Home
-            </Link>
+              Home{" "}
+            </Link>{" "}
           </li>
-
-          {showModeratorBoard && (
-            <li className="nav-item">
-              <Link to={"/mod"} className="nav-link">
-                Moderator Board
-              </Link>
-            </li>
-          )}
-
-          {showAdminBoard && (
-            <li className="nav-item">
-              <Link to={"/admin"} className="nav-link">
-                Admin Board
-              </Link>
-            </li>
-          )}
-
           {currentUser && (
             <li className="nav-item">
               <Link to={"/user"} className="nav-link">
-                User
-              </Link>
+                User{" "}
+              </Link>{" "}
             </li>
-          )}
+          )}{" "}
         </div>
-
         {currentUser ? (
           <div className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link to={"/profile"} className="nav-link">
-                {currentUser.username}
-              </Link>
-            </li>
+                {" "}
+                {currentUser.username}{" "}
+              </Link>{" "}
+            </li>{" "}
             <li className="nav-item">
               <a href="/login" className="nav-link" onClick={logOut}>
-                LogOut
-              </a>
-            </li>
+                LogOut{" "}
+              </a>{" "}
+            </li>{" "}
           </div>
         ) : (
           <div className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
+                Login{" "}
+              </Link>{" "}
             </li>
-
             <li className="nav-item">
               <Link to={"/register"} className="nav-link">
-                Sign Up
-              </Link>
-            </li>
+                Sign Up{" "}
+              </Link>{" "}
+            </li>{" "}
           </div>
-        )}
+        )}{" "}
       </nav>
-
       <div className="container mt-3">
         <Routes>
-          <Route  path={"/"} element={<Home />}></Route>
-          <Route path={"/home"} element={<Home />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
-          <Route exact path="/profile" element={<Profile />}></Route>
-        </Routes>
-      </div>
+          <Route path={"/"} element={<Home />}>
+            {" "}
+          </Route>{" "}
+          <Route path={"/home"} element={<Home />}>
+            {" "}
+          </Route>{" "}
+          <Route exact path="/login" element={<Login />}>
+            {" "}
+          </Route>{" "}
+          <Route exact path="/register" element={<Register />}>
+            {" "}
+          </Route>{" "}
+          <Route exact path="/profile" element={<Profile />}>
+            {" "}
+          </Route>{" "}
+        </Routes>{" "}
+      </div>{" "}
     </div>
   );
 };
